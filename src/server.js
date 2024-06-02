b8744b854298d12fb1304412bab5c94142079ed1
Murilo Damario <murilo.gcdamario@gmail.com>
2024-06-02 19:38:31 -0300

    Create JSON database and implement select and insert methods

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import http from 'node:http'
+import { Database } from './database.js'
 import { json } from './middlewares/json.js'
 
-const tasks = []
+const database = new Database()
 
 const server = http.createServer(async (request, response) => {
   const { method, url } = request
@@ -9,20 +10,25 @@ const server = http.createServer(async (request, response) => {
   await json(request, response)
 
   if (method === 'GET' && url === '/tasks') {
+    const tasks = database.select('tasks')
+
     return response.end(JSON.stringify(tasks))
   }
 
   if (method === 'POST' && url === '/tasks') {
     const { title, description } = request.body
 
-    tasks.push({
+    const task = {
       id: 1,
       title,
       description,
       completed_at: null,
       created_at: new Date(),
       updated_at: new Date()
-    })
+    }
+
+    database.insert('tasks', task)
+
     return response.writeHead(201).end()
   }
 
